c90e0822288d2d2dc6c1167448714c8095aa91e4
sobchuksasha <sobchuksasha1995@gmail.com>
2020-11-10 13:40:10 +0200

    testTask_RN

--- App.js
+++ App.js
@@ -1,21 +1,106 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { View, FlatList, Image, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+let currentPhoto;
+
+function HomeScreen({ navigation }) {
+  const [imgs, setImgs] = useState([]);
+  
+  useEffect(() => {
+    fetch('https://api.unsplash.com/photos/?client_id=Hqdv80bsjO1TypufSbdM4yXLlK15b9kJv01qkyDNrUk')
+      .then(response => response.json())
+      .then(data => setImgs(data));
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <View>
+        <FlatList 
+          data={imgs}
+          renderItem={({item}) => (
+            <View>
+              <TouchableOpacity
+                style={styles.listItem}
+                onPress={() => { 
+                currentPhoto = item.urls.full;
+                navigation.navigate('Photo')}}>
+              <Image
+                style={styles.img}
+                source={{ uri: item.urls.thumb }}
+              />
+
+              <View style={styles.textContainer}>
+                <Text  style={styles.name}>{item.user.id}</Text>
+                <Text style={styles.username}>{ `${item.user.username}`}</Text>
+              </View>
+              </TouchableOpacity>
+            </View>
+          )}
+        />
+      </View>
     </View>
   );
 }
 
+function PhotoScreen({ navigation, fullPhoto }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Image
+        style={styles.imgBig}
+        source={{ uri: currentPhoto }} />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Photo" component={PhotoScreen} />
+
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
+
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  img: {
+    width: 100,
+    height:100,
   },
+
+  imgBig: {
+    width: '100%',
+    height: '100%',
+  },
+
+  listItem: {
+    flexDirection: 'row',
+    padding: 10,
+  },
+
+  name: {
+    fontSize: 20,
+  },
+
+  username: {
+    color: 'gray',
+  },
+
+  textContainer: {
+    paddingLeft: 15,
+  }
+
 });
